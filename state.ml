0250505ed5be929ddbd5ad29f345cbd99080ea9b
Aaron Bembenek <bembenek@g.harvard.edu>
2015-10-15 17:34:39 -0400

    Started to modularize state.

--- state.ml
+++ state.ml
@@ -1,29 +1,59 @@
 open Ast
 
+(* TODO place holder *)
+type value = int
+
 (* thread identifiers *)
 type tid = int
 
-(* vector clocks *)
+(* useful maps *)
 module TidMap = Map.Make(struct type t = tid let compare = compare end)
-type clock = int TidMap.t
-let bot_clock = TidMap.empty
-let lookup_time (id : tid) (t : clock) : int =
-  try TidMap.find id t with Not_found -> 0 
-let inc_clock (id : tid) (t : clock) : clock =
-  TidMap.add id (lookup_time id t + 1) t
-let join_clocks : clock -> clock -> clock =
-  TidMap.fold (fun id time a -> TidMap.add id (max time (lookup_time id a)) a)
+module VarMap = Map.Make(struct type t = var let compare = compare end)
+
+(* vector clocks *)
+module type CLOCK =
+  sig
+    type t 
+    val bot : t 
+    val inc : tid -> t -> t 
+    val join : t -> t -> t 
+  end
+
+module MapClock : CLOCK =
+  struct
+    type t = int TidMap.t
+    let bot = TidMap.empty
+    let lookup (id : tid) (t : t) : int =
+      try TidMap.find id t with Not_found -> 0 
+    let inc (id : tid) (t : t) : t =
+      TidMap.add id (lookup id t + 1) t
+    let join : t -> t -> t =
+      TidMap.fold (fun id time a -> TidMap.add id (max time (lookup id a)) a)
+  end
+
+module Clock = MapClock 
 
 (* memory *)
-module VarMap = Map.Make(struct type t = var let compare = compare end)
-(* TODO change to val *)
-type mem = (int*clock) list VarMap.t
-let empty_mem = VarMap.empty
-let lookup_mem (x : var) (m : mem) : (int*clock) list =
-    try VarMap.find x m with Not_found -> [(0,bot_clock)]
-(* TODO currently stores memory in newest write first order... okay? *)
-let write_mem (x : var) (v : int) (t : clock) (m : mem) : mem =
-    let old = lookup_mem x m in VarMap.add x ((v,t)::old) m
+module type MEM =
+  sig
+    type t
+    val empty : t
+    val lookup : var -> t -> (value*Clock.t) list
+    val write : var -> value -> Clock.t -> t -> t
+  end
+
+module MapMem : MEM =
+  struct
+    type t = (value*Clock.t) list VarMap.t
+    let empty = VarMap.empty
+    let lookup (x : var) (m : t) : (value*Clock.t) list =
+      try VarMap.find x m with Not_found -> [(0,Clock.bot)]
+    (* TODO currently stores memory in newest-write-first order... okay? *)
+    let write (x : var) (v : value) (t : Clock.t) (m : t) : t =
+      let old = lookup x m in VarMap.add x ((v,t)::old) m
+  end
+
+module Mem = MapMem
 
 (* Assumption set *)
 
@@ -33,6 +63,10 @@ let write_mem (x : var) (v : int) (t : clock) (m : mem) : mem =
 
 (* Output config set? *)
 
+(* Thread pool *)
+
+(* Lock state *)
+
 (* SymConfig - record *)
 
-(* SymExec - set *)
+(* SymExec - queue??? *)
